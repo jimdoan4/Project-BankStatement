ce015c94efe013cc06e5e49d51cb18a847013d35
Jim Doan <jimdoan4@gmail.com>
2020-01-31 14:59:11 -0600

    created variable to store content in empty array

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ const list = document.getElementById('list');
 const form = document.getElementById('form');
 const amount = document.getElementById('amount');
 
+// Parsing data and storing content into empty array
+const localStorageTransactions = JSON.parse(
+    localStorage.getItem('transactions')
+);
+
+let transactions =
+    localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+
