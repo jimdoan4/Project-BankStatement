214adfe3bde7dc18f2a0e5268cf08fba507fe42f
Jim Doan <jimdoan4@gmail.com>
2020-01-31 15:07:19 -0600

    created function to update balance

--- script.js
+++ script.js
@@ -97,3 +97,23 @@ ${transaction.desc} <button class="delete-btn" onclick="removeTransaction(${
 
 }
 
+// Update the balance, income and expense
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+
+    const expense = (
+        amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+        -1
+    ).toFixed(2);
+
+    balance.innerText = `$${total}`;
+    money_plus.innerText = `$${income}`;
+    money_minus.innerText = `$${expense}`;
+}
