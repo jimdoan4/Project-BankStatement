4aed92bd48374df9816cb276b8f94444fbcbb7d8
Jim Doan <jimdoan4@gmail.com>
2020-01-31 14:57:34 -0600

    created variables to grab html elements and ids

---
+++ script.js
@@ -0,0 +1,8 @@
+// Grabbing ID's from html tag
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const amount = document.getElementById('amount');
+
