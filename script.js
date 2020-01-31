83dfd628a5b9b74a643f199d0f5cd23a053a1c6f
Jim Doan <jimdoan4@gmail.com>
2020-01-31 15:05:19 -0600

    created functionality to add transaction to empty array

--- script.js
+++ script.js
@@ -14,3 +14,34 @@ const localStorageTransactions = JSON.parse(
 let transactions =
     localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
+// Add transaction
+function addTransaction(e) {
+    // Prevents window from reloading
+    e.preventDefault();
+    // trim() method removes whitespace from both sides of a string.
+    if (text.value.trim() === '' || desc.value.trim() === '' || date.value.trim() === '' || amount.value.trim() === '') {
+        alert('Please add an item, date, amount and description');
+    } else {
+        // Generating ID for each input value
+        const transaction = {
+            id: generateID(),
+            text: text.value,
+            desc: desc.value,
+            date: date.value,
+            amount: +amount.value
+        };
+        // Pushing/Storing input to array
+        transactions.push(transaction);
+
+        addTransactionDOM(transaction);
+
+        updateValues();
+
+        updateLocalStorage();
+
+        text.value = "";
+        amount.value = "";
+        desc.value = "";
+        date.value = "";
+    }
+}
